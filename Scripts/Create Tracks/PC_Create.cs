93caa6a71db7126074bb775c5f52a4b05bb8da9f
Michael Christie <michaelchristie26@gmail.com>
2020-05-18 12:26:57 +0100

    Added some basic player movement, temporary for now

--- Scripts/Create Tracks/PC_Create.cs
+++ Scripts/Create Tracks/PC_Create.cs
@@ -6,6 +6,16 @@ public class PC_Create : MonoBehaviour
 {
     public TrackGenerator TG;
 
+    public enum zoomLevel
+    {
+        x1,
+        x2,
+        x3,
+        x4,
+    }
+
+    public zoomLevel ZoomLevel;
+
     // Update is called once per frame
     void Update()
     {
@@ -14,21 +24,26 @@ public class PC_Create : MonoBehaviour
         {
             AddPoint();
         }
-
-        //moves the player around
-
         //zooms in and out
+        Zoom();
+    }
+
+    private void FixedUpdate()
+    {
+        //moves the player around
+        Vector3 dir = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
+        transform.Translate(dir, Space.World);
     }
 
     void AddPoint()
     {
-        Vector3 newPos = Camera.main.ScreenToWorldPoint(Input.mousePosition + new Vector3(0, 0, 9));
+        Vector3 newPos = Camera.main.ScreenToWorldPoint(Input.mousePosition + new Vector3(0, 0, Camera.main.transform.position.y - 1));
         Debug.Log(newPos);
         TG.AddNewNode(newPos);
     }
 
     void Zoom()
     {
-
+        transform.position = new Vector3(transform.position.x, (int)ZoomLevel * 10f, transform.position.z);
     }
 }
