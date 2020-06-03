dc05108ed6cf4838a992f9e68c9d3ab4ca1aef9a
Michael Christie <michaelchristie26@gmail.com>
2020-06-03 10:39:12 +0100

    Changes to Main Menu UI

---
+++ Scripts/UI/FlexableGridLayout.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FlexableGridLayout : LayoutGroup
+{
+    public enum FitType
+    {
+        Uniform,
+        Width,
+        Height,
+        FixedRows,
+        FixedColums
+    }
+
+    public int rows;
+    public int columns;
+    public Vector2 cellSize;
+    public Vector2 spacing;
+    public FitType fitType;
+
+    public bool fitX;
+    public bool fitY;
+
+    public override void CalculateLayoutInputHorizontal()
+    {
+        base.CalculateLayoutInputHorizontal();
+
+        if (fitType == FitType.Height || fitType == FitType.Width || fitType == FitType.Uniform)
+        {
+            fitX = true;
+            fitY = true;
+
+            float sqrRt = Mathf.Sqrt(transform.childCount);
+            rows = Mathf.CeilToInt(sqrRt);
+            columns = Mathf.CeilToInt(sqrRt);
+        }
+
+        if (fitType == FitType.Width || fitType == FitType.FixedColums)
+        {
+
+            rows = Mathf.CeilToInt(transform.childCount / (float)columns);
+        }
+
+        if (fitType == FitType.Height || fitType == FitType.FixedRows)
+        {
+            columns = Mathf.CeilToInt(transform.childCount / (float)rows);
+        }
+
+        float parentWidth = rectTransform.rect.width;
+        float parentHeight = rectTransform.rect.height;
+
+        float cellWidth = (parentWidth / (float)columns) - ((spacing.x / (float)columns) * 2) - (padding.left / (float)columns) - (padding.right / (float)columns);
+        float cellHeight = (parentHeight / (float)rows) - ((spacing.y / (float)rows) * 2) - (padding.top / (float)rows) - (padding.bottom / (float)rows);
+
+        cellSize.x = fitX ? cellWidth : cellSize.x;
+        cellSize.y = fitY ? cellHeight : cellSize.y;
+
+        int cCount = 0;
+        int rCount = 0;
+
+        for (int i = 0; i < rectChildren.Count; i++)
+        {
+            rCount = i / columns;
+            cCount = i % columns;
+
+            var item = rectChildren[i];
+
+            var xPos = (cellSize.x * cCount) + (spacing.x * cCount) + padding.right;
+            var yPos = (cellSize.y * rCount) + (spacing.y * rCount) + padding.top;
+
+            SetChildAlongAxis(item, 0, xPos, cellSize.x);
+            SetChildAlongAxis(item, 1, yPos, cellSize.y);
+        }
+    }
+
+    public override void CalculateLayoutInputVertical()
+    {
+    }
+
+    public override void SetLayoutHorizontal()
+    {
+
+    }
+
+    public override void SetLayoutVertical()
+    {
+
+    }
+}
