3e67970f8b415e8a626d6d76a38ed144099c4c1c
Michael Christie <michaelchristie26@gmail.com>
2020-06-22 15:33:18 +0100

    Added in UI to the track creator

---
+++ InputUISwitch.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class InputUISwitch : MonoBehaviour
+{
+    public GameObject[] JoypadUIElements;
+    public GameObject[] KeyboardUIElements;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (PlayerInput.GetPlayerByIndex(0).currentControlScheme == "JoyPad")
+        {
+            foreach (GameObject g in JoypadUIElements)
+            {
+                g.SetActive(true);
+            }
+            foreach (GameObject g in KeyboardUIElements)
+            {
+                g.SetActive(false);
+            }
+        }
+        else
+        {
+            foreach (GameObject g in JoypadUIElements)
+            {
+                g.SetActive(false);
+            }
+            foreach (GameObject g in KeyboardUIElements)
+            {
+                g.SetActive(true);
+            }
+        }
+    }
+}
